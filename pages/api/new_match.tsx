a4d80b2c6c5c2eedd89f43471b67a0578f7a50c6
Roman Jackson <rjackson@santandertecnologia.com.ar>
2023-09-12 22:59:17 -0300

    tournaments table

--- pages/api/new_match.tsx
+++ pages/api/new_match.tsx
@@ -1,33 +1,24 @@
 import prisma from "../../lib/prisma";
 
 export default async function handler(req, res) {
-
-    // winner: winner,
-    // tournamentId: id,
-    // player1: player1,
-    // player2: player2
-    const match = JSON.parse(req.body)
-    console.log(match)
-
-
-    if (req.method === "POST") {
+  const match = JSON.parse(req.body);
+  if (req.method === "POST") {
     try {
       const response = await prisma.match.create({
         data: {
-            winner: match.winner,
-            tournamentId: match.tournamentId,
-            players: {
-                connect: [
-                    {
-                        id: match.player1
-                    },
-                    {
-                        id: match.player2
-                    }
-                ]
-            }
-
-        }
+          winner: match.winner,
+          tournamentId: match.tournamentId,
+          players: {
+            connect: [
+              {
+                id: match.player1,
+              },
+              {
+                id: match.player2,
+              },
+            ],
+          },
+        },
       });
       res.status(200).json({ response });
     } catch (e) {
