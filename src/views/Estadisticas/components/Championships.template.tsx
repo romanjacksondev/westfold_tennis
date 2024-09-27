0a977b9691735fd5abc74c8176f64c4433b0efd7
Alejandro Valencia <a@blinktrip.com>
2024-09-27 16:04:29 -0300

    new loader

--- src/views/Estadisticas/components/Championships.template.tsx
+++ src/views/Estadisticas/components/Championships.template.tsx
@@ -1,11 +1,9 @@
 // import { TextHeadingH4 } from "components/Text";
-import { TextHeadingH4 } from 'components/Text';
-import PropTypes from 'prop-types';
-
-
+import LoadingComponent from "components/Loader";
+import { TextHeadingH4 } from "components/Text";
+import PropTypes from "prop-types";
 
 const ChampionshipsTemplate = ({ championships }) => {
-
   interface Player {
     name: string;
     total: number;
@@ -13,20 +11,19 @@ const ChampionshipsTemplate = ({ championships }) => {
       [key: string]: number; // Las claves de `points` son dinámicas (pueden ser "250", "500", etc.) y los valores son números
     };
   }
-  
+
   type PointType = string;
 
   const getAllPointTypes = (data: Player[]): PointType[] => {
     const allPoints: Set<string> = new Set();
-    data.forEach(player => {
-      Object.keys(player.points).forEach(point => {
+    data.forEach((player) => {
+      Object.keys(player.points).forEach((point) => {
         allPoints.add(point);
       });
     });
     return Array.from(allPoints).sort((a, b) => Number(a) - Number(b)); // Ordenar de menor a mayor
   };
 
-  
   const pointTypes = getAllPointTypes(championships);
 
   const Styles = {
@@ -35,9 +32,7 @@ const ChampionshipsTemplate = ({ championships }) => {
   };
   // console.log("championships: ", championships)
   if (championships.length == 0) {
-    return (
-      "LOADING"
-    )
+    return <LoadingComponent size="large" />;
   }
 
   return (
@@ -48,8 +43,10 @@ const ChampionshipsTemplate = ({ championships }) => {
           <tr>
             <th className={Styles.ThStyle}>Nombre</th>
             <th className={Styles.ThStyle}>Total de Torneos Ganados</th>
-            {pointTypes.map(point => (
-              <th className={Styles.ThStyle} key={point}>{point}</th>
+            {pointTypes.map((point) => (
+              <th className={Styles.ThStyle} key={point}>
+                {point}
+              </th>
             ))}
           </tr>
         </thead>
@@ -58,23 +55,21 @@ const ChampionshipsTemplate = ({ championships }) => {
             <tr key={index}>
               <td className={Styles.TdStyle}>{player.name}</td>
               <td className={Styles.TdStyle}>{player.total}</td>
-              {pointTypes.map(point => (
-                <td className={Styles.TdStyle} key={point}>{player.points[point] || 0}</td>
+              {pointTypes.map((point) => (
+                <td className={Styles.TdStyle} key={point}>
+                  {player.points[point] || 0}
+                </td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
     </>
-  )
-
-}
-
-ChampionshipsTemplate.propTypes = {
-  championships: PropTypes.arrayOf(
-    PropTypes.shape({})
-  ).isRequired
+  );
 };
 
+ChampionshipsTemplate.propTypes = {
+  championships: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
 
-export default ChampionshipsTemplate
+export default ChampionshipsTemplate;
--- src/views/InfoPlayer/components/Stats.template.tsx
+++ src/views/InfoPlayer/components/Stats.template.tsx
@@ -1,31 +1,29 @@
-import PropTypes from 'prop-types';
-import MatchTemplate from './MatchStats.template';
-import SetTemplate from './SetStats.template';
-import GameTemplate from './GameStats.template';
+import PropTypes from "prop-types";
+import MatchTemplate from "./MatchStats.template";
+import SetTemplate from "./SetStats.template";
+import GameTemplate from "./GameStats.template";
+import LoadingComponent from "components/Loader";
 
 const StatsTemplate = ({ stats }) => {
-
   if (!stats.matchesPlayed) {
-    return (
-      "LOADING STATS"
-    )
+    return <LoadingComponent size="large" />;
   }
 
   return (
     <>
       <MatchTemplate stats={stats} />
-      <SetTemplate stats={stats}/>
-      <GameTemplate stats={stats}/>
+      <SetTemplate stats={stats} />
+      <GameTemplate stats={stats} />
     </>
-  )
-}
+  );
+};
 
 StatsTemplate.propTypes = {
   stats: PropTypes.shape({
     matchesPlayed: PropTypes.number,
     matchesWon: PropTypes.number,
     matchesLost: PropTypes.number,
-  }).isRequired
+  }).isRequired,
 };
 
-export default StatsTemplate
+export default StatsTemplate;
